ee4d1c138305aa9fac62b80ed2000ed7e6673c74
avide <avide@Lone-wolf57>
2017-10-17 10:49:17 -0400

    commit1

---
+++ lab1.c
@@ -0,0 +1,23 @@
+/*Lab 1 first file - ID number 815006656*/
+#include <p18f452.h>
+
+/*Set configuration bits for use with ICD2*/
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+void main(void)
+{
+    /*insert code from pre-lab Q14 to change values on PORTB*/
+    
+    /*Clear TRISB and PORTB values*/
+    int i;
+    TRISB = 0x00;
+    PORTB = 0x00;
+    
+    for(i = 1; i <16; i++)
+    {
+        PORTB = i;
+    }
+    /*dfgneljdkn*/
+}
